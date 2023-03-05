ef31ef911136e1c958be6fd53f6d1e9e822ab56c
Dicky Joel Saputra <dickyjoelsaputra@gmail.com>
2023-03-06 02:24:06 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+        return view('register');
+    }
+
+    public function registerProcess(Request $request)
+    {
+        $user = User::create([
+            'name' => strtok($request->email, '@'),
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect('/email/verify');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/login', function () {
+    return 'ini login';
+})->name('login');
+Route::get('/register', [AuthController::class, 'register']);
+Route::post('/registerProcess', [AuthController::class, 'registerProcess']);
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/profile');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::get('/profile', function () {
+    return 'user telah terverifikasi';
+})->middleware(['auth', 'verified']);
